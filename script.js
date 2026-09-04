0b6644907989aad0cbd369fb6f64db4d0858bc39
Shrisha <166478099+shrishasat@users.noreply.github.com>
2026-09-04 17:52:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 /* --------------------------------------------------
    SCENE
